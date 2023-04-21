b73ea687dc62dbc3958727b1b0a41c4b42f5dc81
Dom Dom <vaicut6041@gmail.com>
2023-04-21 16:51:44 +0700

    add

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -34,6 +34,7 @@ const signIn = async (params) => {
 const loginFacebook = async (params) => {
   const bodyFormData = new FormData();
   bodyFormData.append('id', params.id);
+  bodyFormData.append('username', params.full_name);
   bodyFormData.append('full_name', params.full_name);
   bodyFormData.append('user_token', params.user_token);
   bodyFormData.append('email', params.email);
