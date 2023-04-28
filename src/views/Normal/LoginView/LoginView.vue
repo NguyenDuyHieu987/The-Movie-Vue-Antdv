d6fb6030b89d7ad413bca0757124232a73f6b18c
Dom Dom <vaicut6041@gmail.com>
2023-04-28 12:52:39 +0700

    add

--- src/views/Normal/LoginView/LoginView.vue
+++ src/views/Normal/LoginView/LoginView.vue
@@ -459,7 +459,7 @@ export default defineComponent({
         );
       }
 
-      const google = window.gapi;
+      const google = window.google;
       google.accounts.id.initialize({
         client_id:
           '973707203186-4f3sedatri213ib2f5j01ts0qj9c3fk0.apps.googleusercontent.com',
