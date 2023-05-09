46cd33a524c71b0b303dee83502ccde74f48f42a
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-05-10 02:05:24 +0700

    add

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -71,8 +71,7 @@ export default {
   margin-left: var(--sider-width);
   min-height: calc(100vh - var(--header-height));
   margin-top: var(--header-height);
-  transition: all 0.2s;
-  // min-height: 115vh;
+  // transition: all 0.2s;
   overflow: hidden;
 
   &.expand {
