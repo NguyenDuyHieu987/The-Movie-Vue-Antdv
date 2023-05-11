bb3acd90bb2b8761ddbc61dab07f593db10c92f1
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-05-11 22:40:22 +0700

    add

--- src/components/TheMenu/TheMenu.vue
+++ src/components/TheMenu/TheMenu.vue
@@ -283,7 +283,7 @@
           height="24px"
           viewBox="0 0 24 24"
           width="24px"
-          fill="#919191"
+          fill="#c5c5c5"
         >
           <g><rect fill="none" height="24" width="24" /></g>
           <g>
