001bbbe19794df3eea1f5ae202bdffacb6bc4af7
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-05-13 18:11:38 +0700

    add

--- src/components/CastCrew/CastCrew.vue
+++ src/components/CastCrew/CastCrew.vue
@@ -83,8 +83,8 @@ export default {
         slideBy: 6,
       },
       1300: {
-        items: 6,
-        slideBy: 6,
+        items: 7,
+        slideBy: 7,
       },
       1400: {
         items: 7,
